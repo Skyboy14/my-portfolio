b2465a375e34e753733b919ad1ba112f04df0a72
Akash Yadav <yadavakash224.ay@gmail.com>
2024-03-23 22:41:11 +0530

    changes

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,47 +7,51 @@ export const Contact = () => {
   return (
     <footer id="contact" className={styles.container}>
       <div className={styles.text}>
-        <h2>Contact</h2>
-        <p>Feel free to reach out!</p>
+        <h1>Contact</h1>
+        <h3>Feel free to reach out!</h3>
       </div>
       <ul className={styles.links}>
         <li className={styles.link}>
-          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
           <a
             href="mailto:myemail@email.com"
             target="_blank"
             rel="noopener noreferrer"
           >
-            yadavakash224.ay@email.com
+            <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+            {/* <h2>yadavakash224.ay@email.com</h2> */}
           </a>
         </li>
         <li className={styles.link}>
-          <img src={getImageUrl("contact/phoneIcon.png")} alt="Phone icon" />
           <a href="tel:+918788775922" target="_blank" rel="noopener noreferrer">
-            +91 878 877 5822
+            <img src={getImageUrl("contact/phoneIcon.png")} alt="Phone icon" />
+            {/* +91 878 877 5822 */}
           </a>
         </li>
         <li className={styles.link}>
-          <img
-            src={getImageUrl("contact/linkedinIcon.png")}
-            alt="LinkedIn icon"
-          />
           <a
             href="https://www.linkedin.com/in/akash-yadav-382008181/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            linkedin.com/AkashYadav
+            {/* linkedin.com/AkashYadav
+             */}
+            <img
+              src={getImageUrl("contact/linkedinIcon.png")}
+              alt="LinkedIn icon"
+            />
           </a>
         </li>
         <li className={styles.link}>
-          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
           <a
             href="https://github.com/Skyboy14"
             target="_blank"
             rel="noopener noreferrer"
           >
-            github.com/AkashYadav
+            <img
+              src={getImageUrl("contact/githubIcon.png")}
+              alt="Github icon"
+            />
+            {/* github.com/AkashYadav */}
           </a>
         </li>
       </ul>
